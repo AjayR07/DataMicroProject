d15d89244054628d9c4be7a16ba30160168ad384
Pranesh <pranesh.vimal5@gmail.com>
2022-01-02 11:06:25 +0530

    CSVReaderService file added

---
+++ src/main/scala/com/presidio/sales/read/CSVReaderService.scala
@@ -0,0 +1,23 @@
+package com.presidio.sales.read
+
+import com.presidio.util.SparkSQLUtility
+
+abstract class CSVReaderService {
+  def read(path: String): Any
+}
+
+class DataFrameReaderService extends CSVReaderService {
+  def read(path: String): Any = {
+    val sparkSession = new SparkSQLUtility().getSession
+    sparkSession.read.format("csv").option("header", "true").load(path)
+  }
+}
+
+class RDDReaderService extends CSVReaderService {
+  def read(path: String): Any = {
+    val sparkContext = new SparkSQLUtility().getSession.sparkContext
+    val textFileRDD = sparkContext.textFile(path)
+    val mappedRDD = textFileRDD.map(each => each.split(","))
+    mappedRDD
+  }
+}
